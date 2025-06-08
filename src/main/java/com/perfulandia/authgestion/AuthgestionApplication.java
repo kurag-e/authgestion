dd65925ea880bfb3cf02ae5688397cc0a7ae8ef4
Valentina <va.fernandezm@duocuc.cl>
2025-06-07 21:03:24 -0400

    gabo callate

---
+++ src/main/java/com/perfulandia/authgestion/AuthgestionApplication.java
@@ -0,0 +1,13 @@
+package com.perfulandia.authgestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthgestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthgestionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/perfulandia/authgestion/AuthgestionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perfulandia.authgestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthgestionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
