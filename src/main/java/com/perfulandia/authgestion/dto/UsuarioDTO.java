627765b808216ed3226f903ac3cedaf6be60aad0
Valentina <va.fernandezm@duocuc.cl>
2025-07-02 01:39:54 -0400

    hateoas dto

--- src/main/java/com/perfulandia/authgestion/dto/UsuarioDTO.java
+++ src/main/java/com/perfulandia/authgestion/dto/UsuarioDTO.java
@@ -3,14 +3,20 @@ package com.perfulandia.authgestion.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.hateoas.RepresentationModel;
+import org.springframework.hateoas.server.core.Relation;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class UsuarioDTO {
+@Relation(collectionRelation = "usuarios")
+public class UsuarioDTO extends RepresentationModel<UsuarioDTO> {
     private Integer idUsuario;
     private String nombreUsuario;
     private String email;
     private String rol;
     private String estado;
+
+    public Integer getId() { return this.idUsuario; }
+
 }
