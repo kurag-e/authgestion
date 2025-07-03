2b979b5f7799aa520f2cfbf12fff54155da6ca82
Valentina <va.fernandezm@duocuc.cl>
2025-07-02 22:15:44 -0400

    zutomayo truth in lies

--- src/main/java/com/perfulandia/authgestion/dto/VendedorDTO.java
+++ src/main/java/com/perfulandia/authgestion/dto/VendedorDTO.java
@@ -1,16 +1,64 @@
 package com.perfulandia.authgestion.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class VendedorDTO {
+
     private Integer idVendedor;
     private Integer idUsuario;
     private String nombreCompleto;
     private String rut;
     private String areaVentas;
+
+    //constructor sin parámetros
+    public VendedorDTO() {
+    }
+
+    //constructor con parámetros
+    public VendedorDTO(Integer idVendedor, Integer idUsuario, String nombreCompleto, String rut, String areaVentas) {
+        this.idVendedor = idVendedor;
+        this.idUsuario = idUsuario;
+        this.nombreCompleto = nombreCompleto;
+        this.rut = rut;
+        this.areaVentas = areaVentas;
+    }
+
+    //getters y setters
+    public Integer getIdVendedor() {
+        return idVendedor;
+    }
+
+    public void setIdVendedor(Integer idVendedor) {
+        this.idVendedor = idVendedor;
+    }
+
+    public Integer getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(Integer idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNombreCompleto() {
+        return nombreCompleto;
+    }
+
+    public void setNombreCompleto(String nombreCompleto) {
+        this.nombreCompleto = nombreCompleto;
+    }
+
+    public String getRut() {
+        return rut;
+    }
+
+    public void setRut(String rut) {
+        this.rut = rut;
+    }
+
+    public String getAreaVentas() {
+        return areaVentas;
+    }
+
+    public void setAreaVentas(String areaVentas) {
+        this.areaVentas = areaVentas;
+    }
 }
