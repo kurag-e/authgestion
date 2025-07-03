d40a9185a3cd9432b24c0fbf93177a8b9254ff80
Valentina <va.fernandezm@duocuc.cl>
2025-07-02 22:13:59 -0400

    tenia q ser lombok del 8

--- src/main/java/com/perfulandia/authgestion/dto/ClienteDTO.java
+++ src/main/java/com/perfulandia/authgestion/dto/ClienteDTO.java
@@ -1,12 +1,5 @@
 package com.perfulandia.authgestion.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class ClienteDTO {
     private Integer idCliente;
     private Integer idUsuario;
@@ -14,4 +7,67 @@ public class ClienteDTO {
     private String rut;
     private String direccion;
     private String telefono;
+
+    //constructor sin parámetros
+    public ClienteDTO() {
+    }
+
+    //constructor con parámetros
+    public ClienteDTO(Integer idCliente, Integer idUsuario, String nombreCompleto, String rut, String direccion, String telefono) {
+        this.idCliente = idCliente;
+        this.idUsuario = idUsuario;
+        this.nombreCompleto = nombreCompleto;
+        this.rut = rut;
+        this.direccion = direccion;
+        this.telefono = telefono;
+    }
+
+    //getters y setters
+    public Integer getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(Integer idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public Integer getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(Integer idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNombreCompleto() {
+        return nombreCompleto;
+    }
+
+    public void setNombreCompleto(String nombreCompleto) {
+        this.nombreCompleto = nombreCompleto;
+    }
+
+    public String getRut() {
+        return rut;
+    }
+
+    public void setRut(String rut) {
+        this.rut = rut;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
 }
