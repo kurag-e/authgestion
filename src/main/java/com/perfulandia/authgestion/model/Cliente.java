011238a534cd34b1856c6443e456f8c38e394f2a
Valentina <va.fernandezm@duocuc.cl>
2025-07-02 22:16:18 -0400

    asdasdas

--- src/main/java/com/perfulandia/authgestion/model/Cliente.java
+++ src/main/java/com/perfulandia/authgestion/model/Cliente.java
@@ -1,23 +1,11 @@
 package com.perfulandia.authgestion.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "clientes")
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class Cliente {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_cliente")
@@ -31,4 +19,67 @@ public class Cliente {
     private String rut;
     private String direccion;
     private String telefono;
+
+    //constructor sin parámetros
+    public Cliente() {
+    }
+
+    //constructor con parámetros
+    public Cliente(Integer idCliente, Usuario usuario, String nombreCompleto, String rut, String direccion, String telefono) {
+        this.idCliente = idCliente;
+        this.usuario = usuario;
+        this.nombreCompleto = nombreCompleto;
+        this.rut = rut;
+        this.direccion = direccion;
+        this.telefono = telefono;
+    }
+
+    //getters y setters
+    public Integer getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(Integer idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getNombreCompleto() {
+        return nombreCompleto;
+    }
+
+    public void setNombreCompleto(String nombreCompleto) {
+        this.nombreCompleto = nombreCompleto;
+    }
+
+    public String getRut() {
+        return rut;
+    }
+
+    public void setRut(String rut) {
+        this.rut = rut;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
 }
