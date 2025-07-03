eeaacc0641cccbae417920136ad43b18f8ad7b5d
Valentina <va.fernandezm@duocuc.cl>
2025-07-02 22:16:47 -0400

    aaa

--- src/main/java/com/perfulandia/authgestion/model/Usuario.java
+++ src/main/java/com/perfulandia/authgestion/model/Usuario.java
@@ -6,20 +6,14 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "usuarios")
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class Usuario {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id_usuario") // <== ¡ESTO ES CLAVE!
+    @Column(name = "id_usuario")
     private Integer idUsuario;
 
     @Column(name = "nombre_usuario")
@@ -28,4 +22,67 @@ public class Usuario {
     private String contrasena;
     private String rol;
     private String estado;
+
+    //constructor sin parámetros
+    public Usuario() {
+    }
+
+    //constructor con parámetros
+    public Usuario(Integer idUsuario, String nombreUsuario, String email, String contrasena, String rol, String estado) {
+        this.idUsuario = idUsuario;
+        this.nombreUsuario = nombreUsuario;
+        this.email = email;
+        this.contrasena = contrasena;
+        this.rol = rol;
+        this.estado = estado;
+    }
+
+    //getters y setters
+    public Integer getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(Integer idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
 }
